0d70f16c4bdf7a116eed48f359c64ab10017bb75
manhinhang <39019063+manhinhang@users.noreply.github.com>
2020-06-12 03:48:06 +0800

    fix: remove hardcoded trade mode

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     account = os.environ['IB_ACCOUNT']
     password = os.environ['IB_PASSWORD']
     trade_mode = os.environ['TRADE_MODE']
-    trade_mode='paper' # paper / live
     ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
     ibc.start()
     ib = IB()
