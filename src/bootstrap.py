e1fa371f9c27ccdd2bd24d8ca8610ddadf1d5639
manhinhang <39019063+manhinhang@users.noreply.github.com>
2020-06-13 16:30:30 +0800

    login timeout if wrong account passed or gateway service is down (#23)
    * feat: #22 add login timeout handle ( 2 minutes )

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,6 +1,26 @@
 from ib_insync import IBC, IB
 import os
 import logging
+import signal
+
+def ping():
+    def timeout_handler(signum, frame):
+        signal.alarm(0)
+        raise TimeoutError('IB gateway timed out, please check your account & password')
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(120)
+
+    ib = IB()
+    while not ib.isConnected():
+        try:
+            IB.sleep(1)
+            ib.connect('localhost', 4001, clientId=1)
+        except (ConnectionRefusedError, OSError) as e:
+            if type(e) is TimeoutError:
+                raise e
+            logging.warning('Still waiting gateway connection..({})'.format(e))
+    
+    ib.disconnect()
 
 if __name__ == "__main__":
     ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
@@ -9,13 +29,6 @@ if __name__ == "__main__":
     trade_mode = os.environ['TRADE_MODE']
     ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
     ibc.start()
-    ib = IB()
-    while not ib.isConnected():
-        try:
-            IB.sleep(1)
-            ib.connect('localhost', 4001, clientId=1)
-        except (ConnectionRefusedError, OSError) as e:
-            logging.warning('Still waiting gateway connection..({})'.format(e))
-    ib.disconnect()
+    ping()
     logging.info('IB gateway is ready.')
     
