0db8cdc8aa623ce48a762ed09b5bc3fc1be1a05d
manhinhang <manhinhang@gmail.com>
2020-06-13 13:21:47 +0800

    feat: #22 add login timeout handle ( 2 minutes )

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,14 +1,15 @@
 from ib_insync import IBC, IB
 import os
 import logging
+import signal
 
-if __name__ == "__main__":
-    ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
-    account = os.environ['IB_ACCOUNT']
-    password = os.environ['IB_PASSWORD']
-    trade_mode = os.environ['TRADE_MODE']
-    ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
-    ibc.start()
+def ping():
+    def timeout_handler(signum, frame):
+        signal.alarm(0)
+        raise TimeoutError('IB gatewau timed out')
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(120)
+
     ib = IB()
     while not ib.isConnected():
         try:
@@ -17,5 +18,14 @@ if __name__ == "__main__":
         except (ConnectionRefusedError, OSError) as e:
             logging.warning('Still waiting gateway connection..({})'.format(e))
     ib.disconnect()
+
+if __name__ == "__main__":
+    ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
+    account = os.environ['IB_ACCOUNT']
+    password = os.environ['IB_PASSWORD']
+    trade_mode = os.environ['TRADE_MODE']
+    ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
+    ibc.start()
+    ping()
     logging.info('IB gateway is ready.')
     
