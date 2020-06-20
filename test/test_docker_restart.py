358fc2fcd64be566e80bd7a2603a6e19ff0a37d0
manhinhang <manhinhang@gmail.com>
2020-06-20 14:52:04 +0800

    fix: #13 add test case

---
+++ test/test_docker_restart.py
@@ -0,0 +1,54 @@
+import pytest
+import os
+import subprocess
+import time
+from ib_insync import IB, util, Forex
+import asyncio
+
+IMAGE_NAME = os.environ['IMAGE_NAME']
+
+def test_ibgw_restart(host):
+    account = os.environ['IB_ACCOUNT']
+    password = os.environ['IB_PASSWORD']
+    trade_mode = os.environ['TRADE_MODE']
+
+    # run a container
+    docker_id = subprocess.check_output(
+        ['docker', 'run', 
+        '--env', 'IB_ACCOUNT={}'.format(account),
+        '--env', 'IB_PASSWORD={}'.format(password),
+        '--env', 'TRADE_MODE={}'.format(trade_mode),
+        '-p', '4002:4002',
+        '-d', IMAGE_NAME, 
+        "tail", "-f", "/dev/null"]).decode().strip()
+    IB.sleep(60)
+    subprocess.check_output(
+        ['docker', 'container', 'stop', docker_id]).decode().strip()
+    subprocess.check_output(
+        ['docker', 'container', 'start', docker_id]).decode().strip()
+    
+    ib = IB()
+    wait = 60
+    while not ib.isConnected():
+        try:
+            IB.sleep(1)
+            ib.connect('localhost', 4002, clientId=999)
+        except (ConnectionRefusedError, OSError, asyncio.exceptions.TimeoutError):
+            pass
+        wait -= 1
+        if wait <= 0:
+            break
+    
+    contract = Forex('EURUSD')
+    bars = ib.reqHistoricalData(
+        contract, endDateTime='', durationStr='30 D',
+        barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+
+    # convert to pandas dataframe:
+    df = util.df(bars)
+    print(df)
+
+    # at the end of the test suite, destroy the container
+    subprocess.check_call(['docker', 'rm', '-f', docker_id])
+
+
