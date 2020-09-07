1ccb2d6f8a0d11418873932835b51f27a8be2759
manhinhang <manhinhang@gmail.com>
2020-09-08 01:23:05 +0800

    feat: add watch dog support

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,31 +1,8 @@
-from ib_insync import IBC, IB
+from ib_insync import IBC, IB, Watchdog
 import os
 import logging
 from ib_account import IBAccount
 import signal
-
-def ping():
-    def timeout_handler(signum, frame):
-        signal.alarm(0)
-        raise TimeoutError('IB gateway timed out, please check your account & password')
-    signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(120)
-
-    ib = IB()
-    pingClientId=int(os.environ['IB_GATEWAY_PING_CLIENT_ID'])
-    maxRetryCount = int(os.environ['ibAccMaxRetryCount'])
-    retryCount = 0
-    while not ib.isConnected():
-        try:
-            IB.sleep(1)
-            ib.connect('localhost', 4001, clientId=pingClientId)
-        except (ConnectionRefusedError, OSError) as e:
-            retryCount += 1
-            if retryCount >= 30:
-                raise ValueError("Invalid ib account") 
-            logging.warning('Still waiting gateway connection..({})'.format(e))
-    
-    ib.disconnect()
 
 if __name__ == "__main__":
     ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
@@ -33,7 +10,13 @@ if __name__ == "__main__":
     password = IBAccount.password()
     trade_mode = IBAccount.trade_mode()
     ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
-    ibc.start()
-    ping()
+    ib = IB()
+    ib.connectedEvent += onConnected
+    def onConnected():
+        print(ib.accountValues())
+    IB_PORT = int(os.environ['IBGW_PORT'])
+    watchdog = Watchdog(ibc, ib, port=IB_PORT)
+    watchdog.start()
+    ib.run()
     logging.info('IB gateway is ready.')
     
