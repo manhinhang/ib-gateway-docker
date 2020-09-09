5ef4b73f9c69a0a2cd5cf3df8d6606fa9b5d1e23
manhinhang <39019063+manhinhang@users.noreply.github.com>
2020-09-10 02:26:21 +0800

    feat: change watchdog timeout value (#43)

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -3,8 +3,11 @@ import os
 import logging
 from ib_account import IBAccount
 import signal
+import sys
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="[%(asctime)s]%(levelname)s:%(message)s")
+    logging.info('start ib gateway...')
     ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
     account = IBAccount.account()
     password = IBAccount.password()
@@ -12,9 +15,17 @@ if __name__ == "__main__":
     ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
     ib = IB()
     def onConnected():
-        print(ib.accountValues())
+        logging.info('IB gateway connected')
+        logging.info(ib.accountValues())
+            
+    def onDisconnected():
+        logging.info('IB gateway disconnected')
     ib.connectedEvent += onConnected
-    watchdog = Watchdog(ibc, ib, port=4001)
+    ib.disconnectedEvent += onDisconnected
+    watchdog = Watchdog(ibc, ib, port=4001, 
+        connectTimeout=30, 
+        appStartupTime=30, 
+        appTimeout=30)
     watchdog.start()
     ib.run()
     logging.info('IB gateway is ready.')
