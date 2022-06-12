0f2aeb3348b53d8a40d020446e3a3fc87d2f2562
manhinhang <39019063+manhinhang@users.noreply.github.com>
2022-06-12 15:13:08 +0800

    chore: show ib gateway info (#48)

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -2,17 +2,29 @@ from ib_insync import IBC, IB, Watchdog
 import os
 import logging
 from ib_account import IBAccount
-import signal
 import sys
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="[%(asctime)s]%(levelname)s:%(message)s")
     logging.info('start ib gateway...')
-    ib_gateway_version = int(os.listdir("/root/Jts/ibgateway")[0])
+    logging.info('---ib gateway info---')
+    twsPath = os.environ['twsPath']
+    logging.info('twsPath', twsPath)
+    gatewayRootPath = "{}/ibgateway".format(twsPath)
+    ib_gateway_version = int(os.listdir(gatewayRootPath)[0])
+    gatewayPath = "{}/{}".format(gatewayRootPath, ib_gateway_version)
+    logging.info("ib gateway version:{}".format(ib_gateway_version))
+    logging.info("ib gateway path:{}".format(gatewayPath))
+    logging.info('-------------------')
     account = IBAccount.account()
     password = IBAccount.password()
     trade_mode = IBAccount.trade_mode()
-    ibc = IBC(ib_gateway_version, gateway=True, tradingMode=trade_mode, userid=account, password=password)
+    ibc = IBC(ib_gateway_version, 
+        gateway=True, 
+        tradingMode=trade_mode, 
+        userid=account, 
+        password=password, 
+        twsPath=twsPath)
     ib = IB()
     def onConnected():
         logging.info('IB gateway connected')
@@ -25,7 +37,26 @@ if __name__ == "__main__":
     watchdog = Watchdog(ibc, ib, port=4001, 
         connectTimeout=30, 
         appStartupTime=30, 
-        appTimeout=30)
+        appTimeout=30,
+        retryDelay=30)
+    def onWatchDogStarting(_):
+        logging.info('WatchDog Starting...')
+    def onWatchDogStarted(_):
+        logging.info('WatchDog Started!')
+    def onWatchDogStopping(_):
+        logging.info('WatchDog Stopping...')
+    def onWatchDogStopped(_):
+        logging.info('WatchDog Stopped!')
+    def onWatchDogSoftTimeout(_):
+        logging.info('WatchDog soft timeout!')
+    def onWatchDogHardTimeoutEvent(_):
+        logging.info('WatchDog hard timeout!')
+    watchdog.startingEvent += onWatchDogStarting
+    watchdog.startedEvent += onWatchDogStarted
+    watchdog.stoppingEvent += onWatchDogStopping
+    watchdog.stoppedEvent += onWatchDogStopped
+    watchdog.softTimeoutEvent += onWatchDogSoftTimeout
+    watchdog.hardTimeoutEvent += onWatchDogHardTimeoutEvent
     watchdog.start()
     ib.run()
     logging.info('IB gateway is ready.')
