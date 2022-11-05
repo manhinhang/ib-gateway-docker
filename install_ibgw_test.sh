8214f6bb2bd0260a0d4e9fd06d5c035a26845823
Ronan-Yann Lorin <ryl@free.fr>
2022-11-05 21:45:14 +0100

    Downgraded to Python 3.8

---
+++ install_ibgw_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 1
+echo "Unpacking JRE ..."
+echo "Starting Installer ..."
+echo ""
+echo "then click Next."
+sleep 1
+echo "Where should IB Gateway 10.20 be installed?"
+echo "[/root/Jts/ibgateway/1020]"
+read x
+sleep 5
+echo "Run IB Gateway 10.20?"
+echo "Yes [y], No [n, Enter]"
+read x
+sleep 1
+exit 0
